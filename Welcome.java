bd824ead90fa4df24805001174a70a8e56437a02
Suchita9096 <suchitashinde18@gmail.com>
2021-08-27 17:51:53 +0530

    Added Git Statement

--- Welcome.java
+++ Welcome.java
@@ -3,6 +3,8 @@ package com.csi.welcome;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to java");
+ 		System.out.println("Welcome to java");
+              System.out.println("Welcome to GitHub");
+               
 	}
 }
